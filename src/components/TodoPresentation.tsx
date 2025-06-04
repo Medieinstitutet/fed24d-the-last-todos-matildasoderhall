86cb04d8a76f9b5ec2512986559a9a2748956a81
matildasoderhall <soderhall.matilda@gmail.com>
2025-06-04 14:51:13 +0200

    :lipstick: show created on todo

--- src/components/TodoPresentation.tsx
+++ src/components/TodoPresentation.tsx
@@ -9,11 +9,11 @@ type TodoPresentaionProps = {
 export const TodoPresentation = ({todo, handleChange, removeTodo}: TodoPresentaionProps) => {
 
     
-
     return <>
         <li className="rounded-md border border-gray-300 bg-lime-100 p-4">
             <article className="flex flex-col gap-2">
-                <div className="flex justify-end">
+                <div className="flex justify-between">
+                    <p className="text-gray-500 text-sm">Created: {new Date(todo.created).toLocaleDateString()}</p>
                     <button className="bg-gray-50 px-3 py-1 rounded" onClick={() => removeTodo(todo.id)}>X</button>
                 </div>
                 <div className="flex items-center gap-2">
