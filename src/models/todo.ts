4054c30b017a08115e0a94e9e2a8186270f4a8f2
matildasoderhall <soderhall.matilda@gmail.com>
2025-06-04 14:46:57 +0200

    :sparkles: add created value for todo class

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,11 +1,14 @@
 export class Todo {
     public id: number;
+    public created: Date;
     constructor(
         public content: string,
         public done: boolean = false,
         public deadline?: string,
-        id?: number
+        id?: number,
+        created?: Date,
     ){
         this.id = id ?? Date.now() + Math.floor(Math.random() * 10000);
+        this.created = created ?? new Date();
     }
 }
